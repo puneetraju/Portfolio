6ce518570600dc0eef51fc0d36aee1c7425bfaf1
Puneet Raju <puneet02ujar@gmail.com>
2025-01-27 01:11:15 +0530

    Update 3.1

--- main.jsx
+++ main.jsx
@@ -2,14 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <BrowserRouter basename='/Portfolio/' >
-            <Routes>
-                <Route path="*" element={<App />}/>
-            </Routes>
-        </BrowserRouter>
-    </StrictMode>
+<StrictMode>
+    <App />
+</StrictMode>,
 )
