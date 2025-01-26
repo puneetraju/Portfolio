c58f12b06eb1f4917c8f4f0da515fff218f94ac0
Puneet Raju <puneet02ujar@gmail.com>
2025-01-26 22:34:01 +0530

    App.jsx

---
+++ App.jsx
@@ -0,0 +1,34 @@
+import Navbar from './src/Components/Navbar';
+import Projects from './src/Components/Projects';
+import Contact from './src/Components/Contact';
+import Skills from './src/Components/Skills';
+import Footer from './src/Components/Footer';
+import About from './src/Components/About';
+import Hero from './src/Components/Hero';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
