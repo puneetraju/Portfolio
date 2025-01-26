65812387c3259f4ad6bbe42e703a93711d5a1492
Puneet Raju <puneet02ujar@gmail.com>
2025-01-27 01:31:42 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Routes, HashRouter, Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
+   
 <StrictMode>
-    <App />
-</StrictMode>,
+    <HashRouter><Routes><Route path="*" element={<App />}/></Routes></HashRouter>
+</StrictMode>
 )
