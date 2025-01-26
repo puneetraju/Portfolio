f9fd4e78cf8f4e73c9367b1bd18ef0dc837c0be2
Puneet Raju <puneet02ujar@gmail.com>
2025-01-27 01:50:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.js'; 
-import { Routes, HashRouter, Route } from 'react-router-dom';
+import { Routes, ReactRouter, Route } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <ReactRouter>
       <Routes>
         <Route path="*" element={<App />} />
       </Routes>
-    </HashRouter>
+    </ReactRouter>
   </StrictMode>
 );
